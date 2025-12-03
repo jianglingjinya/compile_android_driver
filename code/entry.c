4a6705f220042c6073d16240c88247d0241d8e08
jianglingjinya <157221560+jianglingjinya@users.noreply.github.com>
2025-12-03 15:08:56 +0800

    Add files via upload

--- code/entry.c
+++ code/entry.c
@@ -1,51 +1,38 @@
 #include <linux/module.h>
-#include <linux/tty.h>
-#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/kprobes.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
 #include <linux/proc_fs.h>
-#include <linux/list.h>
-#include <linux/kobject.h>
 #include "comm.h"
 #include "memory.h"
 #include "process.h"
 
-#define DEVICE_NAME "PAN"
+#define OP_CMD_READ 0x1001
+#define OP_CMD_WRITE 0x1002
+#define OP_CMD_BASE 0x1003
 
-int dispatch_open(struct inode *node, struct file *file)
+static int handler_ioctl_pre(struct kprobe *p, struct pt_regs *kregs)
 {
-	return 0;
-}
-
-int dispatch_close(struct inode *node, struct file *file)
-{
-	return 0;
-}
-
-long dispatch_ioctl(struct file *const file, unsigned int const cmd, unsigned long const arg)
-{
-	static COPY_MEMORY cm;
-	static MODULE_BASE mb;
-	static char name[0x100] = {0};
-	/*static char key[0x100] = "PANPAN";	
-	static bool is_verified = false;
-
-	if (cmd == OP_INIT_KEY && !is_verified)
-	{
-		if (copy_from_user(key, (void __user *)arg, sizeof(key) - 1) != 0)
-		{
-			return -1;
-		}
-	}*/
+    unsigned int cmd = (unsigned int)kregs->regs[1];
+    unsigned long arg = (unsigned long)kregs->regs[2];
+    static COPY_MEMORY cm;
+    static MODULE_BASE mb;
+    static char name[0x100] = {0};
+    
 	switch (cmd)
 	{
 	case OP_READ_MEM:
 	{
 		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
 		{
-			return -1;
+			return 0;
 		}
 		if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
 		{
-			return -1;
+			return 0;
 		}
 		break;
 	}
@@ -53,11 +40,11 @@ long dispatch_ioctl(struct file *const file, unsigned int const cmd, unsigned lo
 	{
 		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
 		{
-			return -1;
+			return 0;
 		}
 		if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
 		{
-			return -1;
+			return 0;
 		}
 		break;
 	}
@@ -65,56 +52,48 @@ long dispatch_ioctl(struct file *const file, unsigned int const cmd, unsigned lo
 	{
 		if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 || copy_from_user(name, (void __user *)mb.name, sizeof(name) - 1) != 0)
 		{
-			return -1;
+			return 0;
 		}
 		mb.base = get_module_base(mb.pid, name);
 		if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0)
 		{
-			return -1;
+			return 0;
 		}
 		break;
 	}
 	default:
 		break;
 	}
-	return 0;
+    return 0;
 }
 
-struct file_operations dispatch_functions = {
-	.owner = THIS_MODULE,
-	.open = dispatch_open,
-	.release = dispatch_close,
-	.unlocked_ioctl = dispatch_ioctl,
+static struct kprobe kp = {
+    .symbol_name = "inet_ioctl",
+    .pre_handler = handler_ioctl_pre,
+
+    //.symbol_name = "__arm64_sys_lookup_dcookie",
+    //.pre_handler = handler_ioctl_pre,    
 };
 
-struct miscdevice misc = {
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = DEVICE_NAME,
-	.fops = &dispatch_functions,
-};
+static int __init my_module_init(void) {
+    if (register_kprobe(&kp) < 0) {
+        return -1;
+    }
 
-int __init driver_entry(void)
-{
-	int ret;
-	misc.name = get_rand_str();
-	ret = misc_register(&misc);
-    if (ret == 0)
-	{
-		remove_proc_subtree("sched_debug", NULL);
-        remove_proc_entry("uevents_records", NULL);    
-		list_del_rcu(&THIS_MODULE->list);
-		kobject_del(&THIS_MODULE->mkobj.kobj);
-	}
-	return ret;
+	remove_proc_subtree("sched_debug", NULL);
+    remove_proc_entry("uevents_records", NULL);    
+    list_del_rcu(&THIS_MODULE->list);
+	kobject_del(&THIS_MODULE->mkobj.kobj);
+        
+    return 0;
 }
 
-void __exit driver_unload(void)
-{
-	misc_deregister(&misc);
+static void __exit my_module_exit(void) {
+    unregister_kprobe(&kp);
 }
 
-module_init(driver_entry);
-module_exit(driver_unload);
+module_init(my_module_init);
+module_exit(my_module_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("盼盼食品");
